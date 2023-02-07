37b93a6f878202964e65647ac08e9bfc73b01523
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-07 14:09:22 -0300

    feat: criado o arquivo de rotas relacionado a tabela de "categorias" descrita no diagrama

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+import { v4 } from 'uuid'
+
+const categoriesRoutes = Router()
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+
+    const { name, description } = request.body
+    const categorieId = v4()
+    categories.push({
+        categorieId,
+        name, 
+        description,
+        created_at: new Date(),
+    })
+
+    return response.status(201).send()
+})
+
+categoriesRoutes.get("/categories", (request, response) => {
+
+    return response.send(categories)
+})
+
+export { categoriesRoutes }
