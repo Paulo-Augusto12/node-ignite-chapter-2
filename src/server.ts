e0a943be5cee527b7cbbe6e8df0cbf94418bbbd5
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-07 13:47:15 -0300

    chore: adicionada flag no script de iniciar a aplicação para utilizar o debug do próprio vs code

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,19 @@ import express  from "express";
 
 const app = express()
 
-app.listen(3333, () => console.log('server is running'))
+app.use(express.json())
+
 
 app.get('/', (request, response) => {
     response.json({message: 'hello world'})
 })
+
+app.post('/courses', (request, response) => {
+    const { name } = request.body
+    
+    return response.json({ name })
+})
+    
+
+
+app.listen(3333, () => console.log('server is running'))
