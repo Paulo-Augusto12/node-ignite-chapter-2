453345ed5f8a585ca9b834b8d7e62e1e87589828
Paulo Augusto <paulo-augusto12@outlook.com>
2023-02-07 14:20:55 -0300

    format: arquivos formatados utilizando o prettier

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,28 +1,26 @@
-import { Router } from 'express'
-import { v4 as uuidv4 } from 'uuid'
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
 
-const categoriesRoutes = Router()
+const categoriesRoutes = Router();
 
 const categories = [];
 
 categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
 
-    const { name, description } = request.body
+  const category = {
+    categoryId: uuidv4(),
+    name,
+    description,
+    created_at: new Date(),
+  };
+  categories.push(category);
 
-    const category = {
-        categoryId: uuidv4(),
-        name, 
-        description,
-        created_at: new Date(),
-    }
-    categories.push(category)
-
-    return response.status(201).send()
-})
+  return response.status(201).send();
+});
 
 categoriesRoutes.get("/", (request, response) => {
+  return response.send(categories);
+});
 
-    return response.send(categories)
-})
-
-export { categoriesRoutes }
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express  from "express";
+import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/categories',categoriesRoutes)
-    
+app.use("/categories", categoriesRoutes);
 
-app.listen(3333, () => console.log('server is running'))
+app.listen(3333, () => console.log("server is running"));
